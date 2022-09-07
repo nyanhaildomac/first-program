f9a2d7fb4e84c71f6e2689a528ea42a84d1fa18f
nyanhaildomac <113055210+nyanhaildomac@users.noreply.github.com>
2022-09-07 14:11:05 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("hi")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("bye")
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
